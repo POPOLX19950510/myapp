a88f48515030d05e022dc31d4bb9380da2c27060
POPOLX19950510 <32919417+POPOLX19950510@users.noreply.github.com>
2017-12-08 20:21:43 +0800

    初始化myapp

---
+++ myApp/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="header">
+    <ul class="headerNav">
+      <li>home</li>
+      <li>myPicture</li>
+      <li>myText</li>
+      <li>About</li>
+      <li>Contact</li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header'
+  }
+</script>
+
+<style scoped >
+
+</style>
